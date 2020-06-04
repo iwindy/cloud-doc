c6948ec5aa7a6920a545e9fa738a8f188754c41c
xie-jia-fu <203962638@qq.com>
2020-06-04 23:12:06 +0800

    左边内容完善

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { faPlus, faFileImport } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FileSearch from "./components/FilelSearch";
 import FileList from "./components/FileList";
+import BottomBtn from "./components/BottomBtn";
 import defaultFiles from "./utlis/defaultFiles";
 
 function App() {
   return (
-    <div className="App container-fluid">
-      <div className="row">
-        <div className="col-3 bg-light left-panel">
+    <div className="App container-fluid px-0">
+      <div className="row no-gutters">
+        <div className="col-6 bg-light left-panel">
           <FileSearch
             title="我的文档"
             onFileSearch={(value) => {
@@ -28,8 +30,20 @@ function App() {
               console.log(id, newValue);
             }}
           />
+          <div className="row  no-gutters">
+            <div className="col">
+              <BottomBtn text="新建" colorClass="btn-primary" icon={faPlus} />
+            </div>
+            <div className="col">
+              <BottomBtn
+                text="导入"
+                colorClass="btn-success"
+                icon={faFileImport}
+              />
+            </div>
+          </div>
         </div>
-        <div className="col-9 bg-primary right-panel">
+        <div className="col-6 bg-primary right-panel">
           <h1>this right</h1>
         </div>
       </div>
